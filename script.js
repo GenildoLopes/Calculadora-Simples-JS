32cdb75a4afa6549981bea3be4cb5ca7f9b76737
Genildo Lopes <carvalhogenildo800@gmail.com>
2020-01-07 15:33:49 -0300

    Vers. 1.0
    Término da versão

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ function result() {
 
     var numberOne = parseInt(document.getElementById('num-1').value);
     var numberTwo = parseInt(document.getElementById('num-2').value);
-    var res = document.getElementsByTagName('h2');
 
-    var resultado = numberOne / numberTwo;
+    var res = document.getElementById('res-txt');
+
+    var resultNumber = numberOne / numberTwo;
+
+    total = resultNumber.toFixed(2);
+
+    res.innerHTML = `${total}`
     
-    res.innerHTML = `${resultado}`
     
 }
