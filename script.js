ed8e68ccc4f1370d7bbc2b37ebef8ac655d75d4f
Genildo Lopes <carvalhogenildo800@gmail.com>
2020-01-07 09:58:50 -0300

    Errors (console) deleted

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ function result() {
 
     var numberOne = parseInt(document.getElementById('num-1').value);
     var numberTwo = parseInt(document.getElementById('num-2').value);
+    var res = document.getElementsByTagName('h2');
 
-    var resultado = numberOne + numberTwo;
+    var resultado = numberOne / numberTwo;
     
-    box-res.innerHTML = `Res ${resultado}`
+    res.innerHTML = `${resultado}`
     
 }
